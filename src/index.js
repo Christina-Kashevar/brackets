21d343c7101fed694b425e0d379f61c6a40164ed
Christina Kashevar <christi_k@mail.ru>
2020-10-18 11:26:39 +0300

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('')
+  if (arr.length%2 ) { return false;}
+
+  let openingBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i ++) {
+    openingBrackets.push( bracketsConfig[i][0])
+  }
+  let closingBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i ++) {
+    closingBrackets.push( bracketsConfig[i][1])
+  }
+
+  function spliceArr(arr) {
+    if (arr.length ===0 ) return;
+    for (let i = 1; i < arr.length; i ++) {
+        if (closingBrackets.includes(arr[i])) {
+            let index = closingBrackets.indexOf(arr[i]);
+            if (closingBrackets[index] === openingBrackets[index]) {
+                if (arr[i] === arr[i-1]) {
+                    arr.splice(i-1,2);
+                    spliceArr(arr)
+                } else if (arr[i] === arr[i+1]) {
+                    arr.splice(i,2);
+                    spliceArr(arr)
+                } else {
+                    return;
+                }
+            } else if(arr[i-1] === openingBrackets[index]) {
+                arr.splice(i-1,2);
+                spliceArr(arr)  
+              } else {
+                return 
+            }
+        }
+    }
+  }
+
+  spliceArr(arr)
+
+  return arr.length === 0 
 }
+
